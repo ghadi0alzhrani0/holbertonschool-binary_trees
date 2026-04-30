fc67510e6333b6fe83451db185d2a238345851f6
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-04-30 12:41:53 +0000

    0-binary_tree_node.c

---
+++ binary_trees.h
@@ -0,0 +1,28 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - binary tree node
+ * @n: value in the node
+ * @parent: parent node
+ * @left: left child
+ * @right: right child
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
