38dfff6e30830d5487bcc0aa35301eaaf9b688e6
areej-alghamdi <areejaa12@gmail.com>
2026-05-06 05:34:48 +0300

    Fix binary_tree_uncle remove static

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,7 +6,7 @@
  *
  * Return: Pointer to the sibling node, NULL if no sibling
  */
-static binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
